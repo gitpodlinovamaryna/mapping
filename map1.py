1b5d5b5a59072300d8f9863120e4a48a24eb4ff2
maryna <gitpodlinovamaryna@gmail.com>
2021-06-10 19:54:14 +0300

    adding population json dats and styling

--- map1.py
+++ map1.py
@@ -1,11 +1,11 @@
 import folium
 import pandas
-
+import json
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
-name = list(data["NAME"])
+
 
 def color_producer(elevation):
     if elevation <1000:
@@ -23,19 +23,18 @@ def radius_producer(elevation):
     else:
         return 15
 
-html = """
-Volcano name:<br>
-<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
-Height: %s m
-"""
-map = folium.Map(location=[38.58, -99.09],zoom_start=5, tiles = "Stamen Terrain")
 
+map = folium.Map(location=[38.58, -99.09],zoom_start=5, tiles = "Stamen Terrain")
+#Stamen Terrain
 fg = folium.FeatureGroup(name="My Map")
-for lt, ln, el, name in zip(lat, lon, elev, name):
-    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lt, ln],radius = radius_producer(el), popup=folium.Popup(iframe), color = color_producer(el),fill_color =color_producer(el), fill_opacity = 0.7))
-#fg.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m", icon = folium.Icon(color="green")))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln],radius = radius_producer(el), popup=str(el) +" m", color = color_producer(el),fill_color =color_producer(el), fill_opacity = 0.7))
+
+
+
+fg.add_child(folium.GeoJson(data=json.load(open('world.json','r',encoding="utf-8-sig")),style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']< 10000000 else 'orange' if 10000000 <=x['properties']['POP2005']<20000000 else 'red' } ))
+
 
 map.add_child(fg)
-map.save("Map_html_popup_advanced.html")
-#map.save("Map1.html")
+
+map.save("Map1.html")
