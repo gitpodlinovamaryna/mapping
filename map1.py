bd1243c3069522968c2aebe0f81e46b4a418215c
maryna <gitpodlinovamaryna@gmail.com>
2021-06-10 14:59:43 +0300

    change folium.cirlemarker and add radius_produser function

--- map1.py
+++ map1.py
@@ -15,6 +15,13 @@ def color_producer(elevation):
     else:
         return "red"
 
+def radius_producer(elevation):
+    if elevation <1000:
+        return 5
+    elif 1000<=elevation <3000:
+        return 10
+    else:
+        return 15
 
 html = """
 Volcano name:<br>
@@ -26,7 +33,7 @@ map = folium.Map(location=[38.58, -99.09],zoom_start=5, tiles = "Stamen Terrain"
 fg = folium.FeatureGroup(name="My Map")
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = color_producer(el))))
+    fg.add_child(folium.CircleMarker(location=[lt, ln],radius = radius_producer(el), popup=folium.Popup(iframe), color = color_producer(el),fill = True))
 #fg.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m", icon = folium.Icon(color="green")))
 
 map.add_child(fg)
